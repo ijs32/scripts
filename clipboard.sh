fc5ded06f0e065b98b7ebd824b7bd51062248554
ijs32 <iansilber32@gmail.com>
2022-04-06 11:27:55 -0400

    changes to xclip rather than using random node package

--- clipboard.sh
+++ clipboard.sh
@@ -2,4 +2,4 @@
 # this is a very very simple script I am using to create a custom
 # shortcut in ubuntu so I can easily paste my linkedin link on applications and
 # what not
-echo "https://www.linkedin.com/in/iansilber-softwaredeveloper/" | clipboard
+echo "https://www.linkedin.com/in/iansilber-softwaredeveloper/" | xclip -selection c
