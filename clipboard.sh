1efba838173bfd9a325ee76a2698d173861a2c55
ijs32 <iansilber32@gmail.com>
2022-04-06 00:24:20 -0400

    clipboard.sh

--- clipboard.sh
+++ clipboard.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "hello world!"
+# this is a very very simple script I am using to create a custom
+# shortcut in ubuntu so I can easily paste my linkedin link on applications and
+# what not
+echo "https://www.linkedin.com/in/iansilber-softwaredeveloper/" | clipboard
